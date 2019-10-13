433d46184ee87d8d19000e980a05c20f0f79dbac
Gonzalez <gonzalezn@ad.msoe.edu>
2019-10-13 18:59:20 -0500

    Fixed up the cpu, as it wasn't building previously

--- src/cpu/cpu.rs
+++ src/cpu/cpu.rs
@@ -1,15 +1,17 @@
-struct cpu() {   
-    let registers: [u32: 16] = [0:16];
+use crate::{formats::data_processing};
+
+struct CPU {   
+    pub registers: [u32; 16]
 }
 
-impl cpu {
+impl CPU {
     pub fn decode(&mut self, instruction: u32) {
         let opcode: u16 = (((instruction >> 16) & 0xFF0) | ((instruction >> 4) & 0x0F)) as u16;
         match opcode {
             0x080 => { // ADD lli
-                let format: DataProcessing = DataProcessing::from(instruction);
-                },
-                _ => {},
+                let format: data_processing::DataProcessing = data_processing::DataProcessing::from(instruction);
+            },
+            _ => {},
             }
     }
     pub fn fetch() -> u32 {
@@ -18,4 +20,19 @@ impl cpu {
 
 }
 
+// Unit Tests
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_access_registers(){
+        let cpu = CPU{registers: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]};
+        let _empty_registers: [u32; 16] = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+
+        assert_eq!(_empty_registers, cpu.registers);
+
+    }
+
+}
