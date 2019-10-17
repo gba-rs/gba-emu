dda9f3b31bdefd7989d786b1f7c8eb179bac119a
Leo Madda <maddalf@msoe.edu>
2019-10-17 18:18:46 +0000

    Update mod.rs

--- src/formats/mod.rs
+++ src/formats/mod.rs
@@ -1,12 +1,3 @@
-<<<<<<< src/formats/mod.rs
-pub mod data_processing;
-pub mod common;
-pub mod block_data_transfer;
-pub mod branch_exchange;
-pub mod single_data_transfer;
-pub mod multiply;
-pub mod multiply_long;
-=======
 pub mod data_processing;
 pub mod common;
 pub mod block_data_transfer;
@@ -15,4 +6,3 @@ pub mod halfword_register;
 pub mod single_data_transfer;
 pub mod multiply;
 pub mod multiply_long;
->>>>>>> src/formats/mod.rs
