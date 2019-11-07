d3e71e2168e037248ba6928ef603b20f1287fc2f
eric556 <nowace@msoe.edu>
2019-11-07 17:46:05 -0600

    fixed up some of the branch stuff

--- src/formats/branch.rs
+++ src/formats/branch.rs
@@ -13,7 +13,7 @@ pub struct Branch {
 impl From<u32> for Branch {
     fn from(value: u32) -> Branch {
         return Branch {
-            offset: (value & 0xF),
+            offset: (value & 0xFF_FFFF),
             link: ((value & 0x100_0000) >> 24) != 0,
             condition: Condition::from((value & 0xF000_0000) >> 28)
         }
@@ -23,36 +23,21 @@ impl From<u32> for Branch {
 impl Instruction for Branch {
     fn execute(&mut self, cpu: &mut CPU, mem_map: &mut MemoryMap) {
         let current_pc = if cpu.current_instruction_set == InstructionSet::Arm { ARM_PC } else { THUMB_PC };
-        let current_pc_value = cpu.get_register(current_pc);
-        println!("reg: {:X}", cpu.get_register(current_pc));
-        let mut offset = (self.offset >> 2) as u32;
-        println!("offset before {:X}", offset);
+        let current_pc_value = cpu.get_register(current_pc) + 8; // because pipeline bullshit
+        let mut offset = (self.offset << 2) as u32;
 
-        if ((offset >> 23) & 0x1) != 0 {
-            offset = offset | 0xFF00_0000;
+        if ((offset >> 21) & 0x1) != 0 {
+            offset = offset | 0xFFC0_0000;
         }
-        println!("offset: {:X}", offset);
 
         // Setting the link register
         if self.link {
-            cpu.set_register(ARM_LR, current_pc_value); // TODO is this plus 4?
+            // The current PC is 8 ahead but we want to get the next instruction so we subtract 4
+            cpu.set_register(ARM_LR, current_pc_value - 4); 
         }
 
         // Adding the offset to the PC
-        println!("Before current PC us changed: {:X}", cpu.get_register(current_pc));
         let (value, flags) = add(current_pc_value, offset);
         cpu.set_register(current_pc, value);
-        println!("After current PC is set to Value: {:X}", cpu.get_register(current_pc as u8));
-
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_mode(){
-        let a: Branch = Branch::from(0xD12F_FF1F); // ‭11010001 0010 1111 1111 1111 0001 1111‬
     }
 }
