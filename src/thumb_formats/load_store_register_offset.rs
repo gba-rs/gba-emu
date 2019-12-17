d6921cf58edcac50de5254fcb94dcd57e7434a68
Brady Cassada <bradycassada@gmail.com>
2019-12-17 01:26:47 -0600

    added parsing for load store register offset

---
+++ src/thumb_formats/load_store_register_offset.rs
@@ -0,0 +1,66 @@
+use crate::operations::load_store::DataType;
+
+struct LoadStoreRegisterOffset {
+    load: bool,
+    data_type: DataType,
+    offset_register: u8,
+    rb: u8,
+    rd: u8,
+}
+
+impl From<u16> for LoadStoreRegisterOffset {
+    fn from(value: u16) -> LoadStoreRegisterOffset {
+        let data_type: DataType;
+
+        if (value & 0x400) == 0 {
+            data_type = DataType::Word
+        } else {
+            data_type = DataType::Byte
+        }
+        return LoadStoreRegisterOffset {
+            load: ((value & 0x800) >> 11) != 0,
+            data_type,
+            offset_register: ((value & 0x1C0) >> 6) as u8,
+            rb: ((value & 0x38) >> 3) as u8,
+            rd: (value & 0x7) as u8,
+        };
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_creation_0s() {
+        let format = LoadStoreRegisterOffset::from(0x5000);
+
+        assert_eq!(format.load, false);
+        assert_eq!(format.data_type, DataType::Word);
+        assert_eq!(format.offset_register, 0);
+        assert_eq!(format.rb, 0);
+        assert_eq!(format.rd, 0);
+    }
+
+    #[test]
+    fn test_creation() {
+        let format = LoadStoreRegisterOffset::from(0x58B3);
+
+        assert_eq!(format.load, true);
+        assert_eq!(format.data_type, DataType::Word);
+        assert_eq!(format.offset_register, 2);
+        assert_eq!(format.rb, 6);
+        assert_eq!(format.rd, 3);
+    }
+
+    #[test]
+    fn test_creation_byte() {
+        let format = LoadStoreRegisterOffset::from(0x54B3);
+
+        assert_eq!(format.load, false);
+        assert_eq!(format.data_type, DataType::Byte);
+        assert_eq!(format.offset_register, 2);
+        assert_eq!(format.rb, 6);
+        assert_eq!(format.rd, 3);
+    }
+}
--- src/thumb_formats/mod.rs
+++ src/thumb_formats/mod.rs
@@ -1,2 +1,3 @@
 pub mod move_shifted_register;
 pub mod load_store_halfword;
+pub mod load_store_register_offset;
