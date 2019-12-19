5829e97cf1eea1c97e02ecc5a2f81faeedc7c44c
Noe Gonzalez <gonzalezn@msoe.edu>
2019-12-19 01:32:42 +0000

    Resolve "Decode Unconditional Branch"

--- src/thumb_formats/mod.rs
+++ src/thumb_formats/mod.rs
@@ -1,6 +1,7 @@
 pub mod add_subtract;
 pub mod move_shifted_register;
 pub mod load_store_halfword;
+pub mod unconditional_branch;
 pub mod load_store_register_offset;
 pub mod multiple_load_store;
 pub mod software_interrupt;
