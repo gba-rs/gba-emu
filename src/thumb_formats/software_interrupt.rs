9e39c429c15493bc8d34b9be05d30db137be15a5
eric556 <nowace@msoe.edu>
2020-01-13 18:15:41 -0600

    Implemented swi

--- src/thumb_formats/software_interrupt.rs
+++ src/thumb_formats/software_interrupt.rs
@@ -1,8 +1,9 @@
 use crate::operations::instruction::Instruction;
 use crate::memory::memory_map::MemoryMap;
-use crate::cpu::{cpu::CPU, cpu::OperatingMode, condition::Condition};
+use crate::operations::{arm_arithmetic, bitutils::sign_extend_u32};
+use crate::cpu::{cpu::CPU,  cpu::InstructionSet, cpu::OperatingMode, cpu::THUMB_PC, cpu::THUMB_LR};
+use std::fmt;
 
-#[derive(Debug)]
 pub struct ThumbSoftwareInterrupt {
     pub comment_immediate: u8
 }
@@ -12,5 +13,57 @@ impl From<u16> for ThumbSoftwareInterrupt {
         return ThumbSoftwareInterrupt{
             comment_immediate: (value & 0xFF) as u8
         };
+    }
+}
+
+impl Instruction for ThumbSoftwareInterrupt {
+    fn execute(&self, cpu: &mut CPU, _mem_map: &mut MemoryMap) {
+        cpu.set_spsr(cpu.cpsr);
+        let current_pc = cpu.get_register(THUMB_PC);
+        cpu.set_register(THUMB_LR, current_pc + 4); // set LR to the next instruction        
+        cpu.set_register(THUMB_PC, 0x08);
+        cpu.current_instruction_set = InstructionSet::Arm;
+        cpu.operating_mode = OperatingMode::Supervisor;
+    }
+
+    fn asm(&self) -> String{
+        return format!("{:?}", self);
+    }
+}
+
+impl fmt::Debug for ThumbSoftwareInterrupt {
+    fn fmt( & self, f: & mut fmt::Formatter < '_ > ) -> fmt::Result {
+        write!(f, "SWI {}", self.comment_immediate)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::gba::GBA;
+    use crate::cpu::{cpu::InstructionSet, cpu::THUMB_PC, cpu::ARM_PC, cpu::ARM_LR};
+    use std::borrow::{BorrowMut};
+
+    #[test]
+    fn swi_test() {
+        let mut gba: GBA = GBA::default(); 
+        gba.cpu.current_instruction_set = InstructionSet::Thumb;
+
+        gba.cpu.set_register(THUMB_PC, 24);
+
+        let decode_result = gba.cpu.decode(0xDF00);
+        match decode_result {
+            Ok(mut instr) => {
+                (instr.borrow_mut() as &mut dyn Instruction).execute(&mut gba.cpu, &mut gba.mem_map);
+            },
+            Err(e) => {
+                panic!("{:?}", e);
+            }
+        }
+
+        assert_eq!(InstructionSet::Arm, gba.cpu.current_instruction_set);
+        assert_eq!(OperatingMode::Supervisor, gba.cpu.operating_mode);
+        assert_eq!(0x8, gba.cpu.get_register(ARM_PC));
+        assert_eq!(28, gba.cpu.get_register(ARM_LR));
     }
 }
