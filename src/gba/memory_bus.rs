09a7e7918e0dcd331995d763b206f6ae950c173a
Brady <cassadab@msoe.edu>
2020-02-03 14:26:37 -0600

    Added wrappers for mem_map write methods

--- src/gba/memory_bus.rs
+++ src/gba/memory_bus.rs
@@ -29,4 +29,19 @@ impl MemoryBus {
         self.cycle_clock.update_cycles(address, MemAccessSize::Mem32, CycleType::N);
         self.mem_map.read_u32(address)
     }
+
+    pub fn write_u8(&mut self, address: u32, value: u8) {
+        self.cycle_clock.update_cycles(address, MemAccessSize::Mem8, CycleType::N);
+        self.mem_map.write_u8(address, u8);
+    }
+
+    pub fn write_u16(&mut self, address: u32, value: u16) {
+        self.cycle_clock.update_cycles(address, MemAccessSize::Mem16, CycleType::N);
+        self.mem_map.write_u16(address, value);
+    }
+
+    pub fn write_u32(&mut self, address: u32, value: u32) {
+        self.cycle_clock.update_cycles(address, MemAccessSize::Mem32, CycleType::N);
+        self.mem_map.write_u32(address, value);
+    }
 }
