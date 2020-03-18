f3f0cc43753fb5e1e078d651e102c9ec10313b70
Gonzalez <gonzalezn@ad.msoe.edu>
2020-03-18 10:54:39 -0500

    WIP still

--- src/memory/timer_registers.rs
+++ src/memory/timer_registers.rs
@@ -3,14 +3,14 @@ use wasm_bindgen::__rt::std::rc::Rc;
 use memory_macros::*;
 use super::GbaMem;
 
-#[multiple_memory_segment(2, 0x4000100, 0x4000104, 0x4000108,0x400010C)]
+#[multiple_memory_segment(2, 0x4000100, 0x4000104, 0x4000108, 0x400010C)]
 #[bit_field(data, 0, 15)]
 pub struct TimerDataRegister {
     pub memory: Rc<RefCell<GbaMem>>,
     pub index: usize
 }
 
-#[multiple_memory_segment(2, 0x4000102, 0x4000106, 0x400010A,0x400010E)]
+#[multiple_memory_segment(2, 0x4000102, 0x4000106, 0x400010A, 0x400010E)]
 #[bit_field(pre_scalar_selection, 0,2)]
 #[bit_field(count_up_enable, 2,1)]
 #[bit_field(timer_irq_enable, 6,1)]
--- src/timers/timer.rs
+++ src/timers/timer.rs
@@ -27,34 +27,38 @@ impl TimerHandler {
                 Timer {
                     timer: TimerDataRegister::new(0),
                     controller: TimerControlRegister::new(0),
-                    initial_value: 0
+                    initial_value: 0,
+                    cycles: 0
                 },
                 Timer {
                     timer: TimerDataRegister::new(1),
                     controller: TimerControlRegister::new(1),
-                    initial_value: 0
+                    initial_value: 0,
+                    cycles: 0
 
                 },
                 Timer {
                     timer: TimerDataRegister::new(2),
-                    controller: TimerControlRegister::new   (2),
-                    initial_value: 0
+                    controller: TimerControlRegister::new(2),
+                    initial_value: 0,
+                    cycles: 0
                 },
                 Timer {
                     timer: TimerDataRegister::new(3),
                     controller: TimerControlRegister::new(3),
-                    initial_value: 0
+                    initial_value: 0,
+                    cycles: 0
                 },
             ]
         }
     }
-    fn frequency(&self) -> usize {
-        match self.controller.pre_scalar_selection {
+    fn frequency(&self, timer_number: usize) -> usize {
+        match self.timers[timer_number].controller.get_pre_scalar_selection() {
             0 => 1,
             1 => 64,
             2 => 256,
             3 => 1024,
-            _ => err!(),
+            _ => panic!("Error in processing frequency")
         }
     }
 
@@ -63,16 +67,19 @@ impl TimerHandler {
             self.timers[i].register(mem);
         }
     }
-    pub fn write_to_register(&mut self, timer_number: usize, initial_value: u16){
-        self.timers[timer_number].initial_value = initial_value;
+
+    pub fn write_to_register(&mut self, _timer_number: usize, initial_value: u16){
+        self.timers[_timer_number].initial_value = initial_value;
     }
-    pub fn update(&mut self, cycles: usize, timer_number: usize ){
-        let num_overflows = 0;
-        let freq = self.frequency();
+
+    pub fn update(&mut self, _current_cycles: usize, timer_number: usize ){
+        let _overflows = 0;
+        let _freq = self.frequency(timer_number);
 
 
     }
-    pub fn set_enable(&mut self, timer_number: usize){
+    
+    pub fn set_enable(&mut self, _timer_number: usize){
         //TODO
     }
 
