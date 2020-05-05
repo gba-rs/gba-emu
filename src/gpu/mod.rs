f9fd76635ead70b5e64bb54bdf5a8409339eceba
Eric Nowac <enowac@gmail.com>
2020-05-05 18:00:23 -0500

    Removed tile_map_entry file and moved the struct into the tile_map file

--- src/gpu/mod.rs
+++ src/gpu/mod.rs
@@ -1,6 +1,5 @@
 pub mod gpu;
 pub mod rgb15;
-pub mod tile_map_entry;
 pub mod graphic_effects;
 pub mod object;
 pub mod tile_map;
--- src/gpu/tile_map.rs
+++ src/gpu/tile_map.rs
@@ -7,9 +7,27 @@ use crate::{
 };
 use super::{
     gpu::{GPU, DISPLAY_WIDTH}, 
-    rgb15::Rgb15, 
-    tile_map_entry::TileMapEntry
+    rgb15::Rgb15
 };
+
+#[derive(Debug)]
+pub struct TileMapEntry {
+    pub tile_index: u16,
+    pub vertical_flip: bool,
+    pub horizontal_flip: bool,
+    pub palette_bank: u8
+}
+
+impl From<u16> for TileMapEntry {
+    fn from(value: u16) -> TileMapEntry {
+        return TileMapEntry {
+            tile_index: (value & 0x3FF) as u16,
+            vertical_flip: ((value >> 10) & 0x1) != 0,
+            horizontal_flip: ((value >> 11) & 0x1) != 0,
+            palette_bank: (value >> 12) as u8
+        };
+    }
+}
 
 impl GPU {
     pub fn render_bg(&mut self, mem_map: &mut MemoryMap, bg_number: usize) {
